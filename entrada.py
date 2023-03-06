8a5fd292a10d1bb6461b2de1b6f9a4d62cb024d0
Edgar-Novoa <za18011207@zapopan.tecmm.edu.mx>
2023-03-06 15:17:31 -0600

    entrada.py

--- entrada.py
+++ entrada.py
@@ -3,16 +3,35 @@ import tkinter as tk
 from tkinter import *
 import cv2
 
-
-
-
 ventana: Tk = tk.Tk()
 ventana.title("ASISTENCIA")
 ventana.geometry("700x600+500+50")
 ventana.resizable(width=False, height=False)
 
+
+#funcion camara
+def v2():
+
+    '''Importamos el script .xml'''
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        _, img = cap.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.imshow('img', img)
+        k = cv2.waitKey(30)
+        if k == 27:  # 27 es el ascii para esc
+            break
+    cap.release()
+
+
 #boton
-boton = tkinter.Button(ventana,text="INICIAR")
+boton = tkinter.Button(ventana,text="INICIAR",command = v2)
 boton.place(x=300, y=500)
 
 ventana.mainloop()
