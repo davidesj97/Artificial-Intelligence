2532defc7d45a0eb500ddb2b6fc37a637894070a
Edgar-Novoa <za18011207@zapopan.tecmm.edu.mx>
2023-03-06 15:08:07 -0600

    entrada.py

--- entrada.py
+++ entrada.py
@@ -1,7 +1,18 @@
+import tkinter
 import tkinter as tk
+from tkinter import *
+import cv2
 
-ventana = tk.Tk()
+
+
+
+ventana: Tk = tk.Tk()
 ventana.title("ASISTENCIA")
-ventana.geometry("900x600+500+50")
+ventana.geometry("700x600+500+50")
+ventana.resizable(width=False, height=False)
+
+#boton
+boton = tkinter.Button(ventana,text="INICIAR")
+boton.place(x=300, y=500)
 
 ventana.mainloop()
