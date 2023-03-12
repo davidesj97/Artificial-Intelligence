7f7e5fa9a59302c92acf2dc206b4b60630c27186
alanluna <za18011658@zapopan.tecmm.edu.mx>
2023-03-11 22:07:24 -0600

    Correction for Grammar.

--- entrada.py
+++ entrada.py
@@ -9,9 +9,8 @@ ventana.geometry("300x300")
 ventana.resizable(width=False, height=False)
 
 
-#funcion camara
+# funcion camara
 def v2():
-
     '''Importamos el script .xml'''
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -23,18 +22,18 @@ def v2():
         faces = face_cascade.detectMultiScale(gray, 1.1, 4)
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.imshow('img', img)
+        cv2.imshow('Camera', img)
         k = cv2.waitKey(30)
-        if k == 27:  # 27 es el ascii para esc
+        if k == 27:  # 27 es el Ascii para Esc
             break
     cap.release()
 
 
-#boton
-boton = tkinter.Button(ventana,text="INICIAR",command = v2)
+# boton
+boton = tkinter.Button(ventana, text="INICIAR", command=v2)
 boton.place(x=50, y=100)
 
-Boton2 = tkinter.Button(ventana,text="ASISTENCIAS")
+Boton2 = tkinter.Button(ventana, text="ASISTENCIAS")
 Boton2.place(x=150, y=100)
 
 ventana.mainloop()
