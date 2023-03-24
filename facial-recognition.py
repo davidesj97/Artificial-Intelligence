fc0f389137286ddc2a444852b20e4c0843ccdb55
imac <za18011658@zapopan.tecmm.edu.mx>
2023-03-23 19:53:12 -0600

    Kivy

---
+++ facial-recognition.py
@@ -0,0 +1,27 @@
+import cv2
+from kivy.app import App
+from kivy.uix.button import Button
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.imshow('Facial-Recognition', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+
+
+class boton(App):
+    def build(self):
+        return Button(text='Ejemplo Botón')
+
+
+if __name__ == '__main__':
+    boton().run()
