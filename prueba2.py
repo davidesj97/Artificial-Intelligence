f4d012ef1482d31e1b50a7e812fc1e2a05b0649c
Edgar-Novoa <za18011207@zapopan.tecmm.edu.mx>
2023-03-30 20:41:15 -0600

    edgar

--- prueba2.py
+++ prueba2.py
@@ -3,26 +3,14 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 import cv2
 import Asistencia
+import camara
+
 
 class interfaz1(BoxLayout):
     def bIniciar(self, instance):
-        '''Importamos el script .xml'''
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-        cap = cv2.VideoCapture(0)
-
-        while True:
-            _, img = cap.read()
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            cv2.imshow('Camera', img)
-            k = cv2.waitKey(30)
-            if k == 27:  # 27 es el Ascii para Esc
-                break
-        cap.release()
-
+        App.get_running_app().stop()
+        # corre interfaz asistencia
+        camara.ejecucioncamara().run()
 
     def bAsistencia(self, instance):
         App.get_running_app().stop()
