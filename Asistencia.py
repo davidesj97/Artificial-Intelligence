3706d7df1cf96e4a45e308f6ed8f5efa155ba3c1
Edgar-Novoa <za18011207@zapopan.tecmm.edu.mx>
2023-04-03 18:46:12 -0600

    edgar

--- Asistencia.py
+++ Asistencia.py
@@ -1,25 +1,32 @@
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+import tkinter as tk
+from tkinter import ttk
+import entrada
+def interfazasistecia():
+    # Creamos la ventana principal
+    ventana2 = tk.Tk()
+    ventana2.title("ASISTENCIAS")
 
+    # Creamos la tabla con 4 columnas y filas
+    table = ttk.Treeview(ventana2, columns=("ID", "Nombre", "Apellido Paterno", "Apellido Materno"), show="headings")
 
+    # Establecemos los encabezados de cada columna
+    table.heading("ID", text="ID")
+    table.heading("Nombre", text="Nombre")
+    table.heading("Apellido Paterno", text="Apellido Paterno")
+    table.heading("Apellido Materno", text="Apellido Materno")
+    table.pack(padx=10, pady=10)
 
+    for i in range(6):
+        table.insert("", tk.END, values=(f"{i + 1}", f"Nombre {i + 1}", f"Apellido Paterno {i + 1}",f"Apellido Materno {i + 1}"))
 
-def form_asistencias():
-    class MyInterface(BoxLayout):
+    # Creamos los botones para salir y regresar
+    exit_button = tk.Button(ventana2, text="SALIR", command=ventana2.quit)
+    exit_button.pack(pady=10)
+    exit_button = tk.Button(ventana2, text="REGRESAR", command= lambda: [ventana2.destroy(), entrada.interfazinicial()])
+    exit_button.pack(pady=10)
 
-        def __init__(self, **kwargs):
-            super(MyInterface, self).__init__(**kwargs)
+    return ventana2
 
-            # agregar widgets a la interfaz
-            self.add_widget(Label(text="ASISTENCIAS"))
-
-    class MyApp(App):
-
-        def build(self):
-            return MyInterface()
-
-    MyApp().run()
-
-if __name__ == '__main__':
-    form_asistencias()
+if __name__ == "__main__":
+    asistencia = interfazasistecia()
+    asistencia.mainloop()
--- camara.py
+++ camara.py
@@ -1,37 +1,32 @@
 import cv2
 
-
 def ejecucioncamara():
-#carga el clasificador frontal para detección de rostros
+    # carga el clasificador frontal para detección de rostros
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+    # inicia la captura de video desde la cámara
+    cap = cv2.VideoCapture(0)
 
-    '''Importamos el script .xml'''
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    while True:
+        # lee un frame de la captura de video
+        ret, frame = cap.read()
 
-# inicia la captura de video desde la cámara
-cap = cv2.VideoCapture(0)
+        # convierte el frame a escala de grises para la detección de rostros
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-while True:
-    # lee un frame de la captura de video
-    ret, frame = cap.read()
+        # detecta rostros en el frame
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # convierte el frame a escala de grises para la detección de rostros
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # dibuja un rectángulo alrededor de cada rostro detectado
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # detecta rostros en el frame
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        # muestra el frame resultante
+        cv2.imshow('ASISTENCIA', frame)
 
-    # dibuja un rectángulo alrededor de cada rostro detectado
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        # espera por la tecla 'q' para salir
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-    # muestra el frame resultante
-    cv2.imshow('Detección de rostros', frame)
-
-    # espera por la tecla 'q' para salir
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-# libera la captura de video y cierra la ventana
-cap.release()
-
+    # libera la captura de video y cierra la ventana
+    cap.release()
