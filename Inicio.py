57c885259579d72b6afc34bf3f1d938de6314688
Edgar-Novoa <za18011207@zapopan.tecmm.edu.mx>
2023-04-15 14:46:58 -0600

    edgar

---
+++ Inicio.py
@@ -0,0 +1,173 @@
+from tkinter import *
+import tkinter as tk
+import cv2
+from matplotlib import pyplot
+from mtcnn.mtcnn import MTCNN
+import Asistencia
+
+
+
+# Funcion para almacenar el registro facial
+
+def registro_facial():
+    # Vamos a capturar el rostro
+    cap = cv2.VideoCapture(0)  # Elegimos la camara con la que vamos a hacer la deteccion
+    while (True):
+        ret, frame = cap.read()  # Leemos el video
+        cv2.imshow('Registro Facial', frame)  # Mostramos el video en pantalla
+        if cv2.waitKey(1) == 27:  # Cuando oprimamos "Escape" rompe el video
+            break
+
+        # Detección de rostros
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convertimos la imagen a escala de grises
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")  # Cargamos el clasificador de detección de rostros
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,minSize=(30, 30))  # Detectamos los rostros en la imagen
+
+        if len(faces) == 0:  # Si no se detecta ningún rostro, mostramos un mensaje de error
+            error_label = Label(pantalla1, text="No se detectó ningún rostro", fg="red", font=("Calibri", 11))
+            error_label.pack()
+            cap.release()  # Cerramos
+            cv2.destroyAllWindows()
+            return
+
+    usuario_img = nombre.get()
+    cv2.imwrite(usuario_img + ".jpg",
+                frame)  # Guardamos la ultima caputra del video como imagen y asignamos el nombre del usuario
+    cap.release()  # Cerramos
+    cv2.destroyAllWindows()
+
+    nombre_entrada.delete(0, END)  # Limpiamos los text variables
+    apellidoP_entrada.delete(0, END)
+    apellidoM_entrada.delete(0, END)
+
+    Label(pantalla1, text="Registro Facial Exitoso", fg="green", font=("Calibri", 11)).pack()
+
+    # Detectamos el rostro y exportamos los pixeles
+
+    def reg_rostro(img, lista_resultados):
+        data = pyplot.imread(img)
+        for i in range(len(lista_resultados)):
+            x1, y1, ancho, alto = lista_resultados[i]['box']
+            x2, y2 = x1 + ancho, y1 + alto
+            pyplot.subplot(1, len(lista_resultados), i + 1)
+            pyplot.axis('off')
+            cara_reg = data[y1:y2, x1:x2]
+            cara_reg = cv2.resize(cara_reg, (150, 200),interpolation=cv2.INTER_CUBIC)  # Guardamos la imagen con un tamaño de 150x200
+            cv2.imwrite(usuario_img + ".jpg", cara_reg)
+            pyplot.imshow(data[y1:y2, x1:x2])
+        pyplot.show()
+
+    img = usuario_img + ".jpg"
+    pixeles = pyplot.imread(img)
+    detector = MTCNN()
+    caras = detector.detect_faces(pixeles)
+    reg_rostro(img, caras)
+
+
+# funcion interfaz registros
+def registro():
+    global nombre
+    global apellidoP  # Globalizamos las variables para usarlas en otras funciones
+    global apellidoM
+    global nombre_entrada
+    global apellidoP_entrada
+    global apellidoM_entrada
+    global pantalla1
+    pantalla1 = Toplevel(pantalla)  # Esta pantalla es de un nivel superior a la principal
+    pantalla1.title("Registros Usuarios")
+    pantalla1.geometry("300x250")  # Asignamos el tamaño de la ventana
+
+    # --------- Empezaremos a crear las entradas ----------------------------------------
+
+    nombre = StringVar()
+    apellidoP = StringVar()
+    apellidoM = StringVar()
+
+
+    Label(pantalla1, text="NOMBRE * ").pack()  # Mostramos en la pantalla 1 el usuario
+    nombre_entrada = Entry(pantalla1, textvariable=nombre)  # Creamos un text variable para que el usuario ingrese la info
+    nombre_entrada.pack()
+
+    Label(pantalla1, text="APELLIDO PATERNO * ").pack()  # Mostramos en la pantalla 1 la contraseña
+    apellidoP_entrada = Entry(pantalla1, textvariable=apellidoP)  # Creamos un text variable para que el usuario ingrese la contra
+    apellidoP_entrada.pack()
+
+    Label(pantalla1, text="APELLIDO MATERNO * ").pack()  # Mostramos en la pantalla 1 la contraseña
+    apellidoM_entrada = Entry(pantalla1,textvariable=apellidoM)  # Creamos un text variable para que el usuario ingrese la contra
+    apellidoM_entrada.pack()
+
+    Label(pantalla1, text="").pack()  # Dejamos un espacio para la creacion del boton
+
+    # ------------ Vamos a crear el boton para hacer el registro facial --------------------
+    Label(pantalla1, text="").pack()
+    Button(pantalla1, text="Registro Facial", width=15, height=1, command=registro_facial).pack()
+
+
+
+
+# Funcion camara para la asistencia Facial
+def cam_asistencia_facial():
+    # capturar el rostro
+    cap = cv2.VideoCapture(0)  # Elegimos la camara
+    while (True):
+        ret, frame = cap.read()  # Leemos el video
+        cv2.imshow(' Facial', frame)  # Mostramos el video en pantalla
+        if cv2.waitKey(1) == 27:  # Cuando oprimamos "Escape" rompe el video
+            break
+
+    cap.release()  # Cerramos
+    cv2.destroyAllWindows()
+
+
+
+
+    # Funcion para guardar el rostro
+
+
+def Iasistencia():
+        global pantalla2
+
+
+        pantalla2 = Toplevel(pantalla)
+        pantalla2.title("ASISTENCIA")
+        pantalla2.geometry("300x250")  # Creamos la ventana
+
+
+        # boton inicio de asistencia
+
+        Label(pantalla2, text="").pack()
+        Button(pantalla2, text="Inicio de Asistencia", width=20, height=1, command=cam_asistencia_facial).pack()
+
+
+
+#Funcion pantalla principal
+
+def pantalla_principal():
+    global pantalla  # Globalizamos la variable para usarla en otras funciones
+    pantalla = Tk()
+    pantalla.configure(bg="#515A5A")
+    pantalla.geometry("400x350")  # Asignamos el tamaño de la ventana
+    pantalla.title("Reconocimiento facial")  # Asignamos el titulo de la pantalla
+
+
+    #Vamos a Crear los Botones
+
+
+
+    bIniciar = tk.Button(pantalla, text="Iniciar Asistencia",height = "1", width = "20", command=Iasistencia)
+    bIniciar.pack(pady=30)
+
+    bRegistroU = tk.Button(pantalla, text="Registrar Usuario",height = "1", width = "20",command=registro)
+    bRegistroU.pack(pady=30)
+
+    bAsistir = tk.Button(pantalla, text="Asistencias",height = "1", width = "20", command= lambda: [pantalla.destroy(), Asistencia.interfazasistecia()])
+    bAsistir.pack(pady=30)
+
+
+    bSalir = tk.Button(pantalla, text="Salir",height = "1", width = "20", command=pantalla.destroy)
+    bSalir.pack(pady=30)
+
+    pantalla.mainloop()
+
+if __name__ == "__main__":
+ pantalla_principal()
