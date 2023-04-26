fa8d7928fc8afc5aa722d5ab4be3ff6d74ce917e
Edgar-Novoa <za18011207@zapopan.tecmm.edu.mx>
2023-04-26 14:14:10 -0600

    edgar

---
+++ IniciarA.py
@@ -0,0 +1,18 @@
+
+import cv2
+
+
+# Funcion ejecucion camara para la asistencia Facial
+def cam_asistencia_facial():
+    # capturar el rostro
+    cap = cv2.VideoCapture(0)  # Elegimos la camara
+    while (True):
+        ret, frame = cap.read()  # Leemos el video
+        cv2.imshow(' Facial', frame)  # Mostramos el video en pantalla
+        if cv2.waitKey(1) == 27:  # Cuando oprimamos "Escape" rompe el video
+            break
+
+    cap.release()  # Cerramos
+    cv2.destroyAllWindows()
+
+
